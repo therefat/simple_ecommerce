44e251f6c6a16051d0f1722ea0a20c3bef749c5e
mdrefathossen <refathossen2001@gmail.com>
2023-10-30 15:29:05 +0600

    database schema desing done

---
+++ Users.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password:{
+        type: String,
+        required: true,
+        minLength: 7,
+        trim: true,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('password must contain password')
+            }
+        }
+    },
+    tokens: [{
+        token : {
+            type: String,
+            required: true
+        }
+    }]
+},{
+    timestamps: true
+})
+const Users = mongoose.model('Users', userSchema)
+module.exports = Users;
