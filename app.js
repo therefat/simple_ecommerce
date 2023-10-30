04856ffb713ec6c6aac92b00a3ee36047bde5613
mdrefathossen <refathossen2001@gmail.com>
2023-10-30 21:13:33 +0600

    routes models added main file

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const userRouter = require('./routers/user')
+const itemRouter = require('./routers/item')
+const cartRouter = require('./routers/cart')
 require('./db/mongoose')
 const app = express()
 app.use(express.json())
+app.use(userRouter)
+app.use(itemRouter)
+app.use(cartRouter)
 
 const port = process.env.PORT
 console.log(port)
